6f94574311cfdd8654d3793fa1ee170183229711
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2026-08-25 09:05:28 -0500

    feat: initial stlc build
    Stainless-Generated-From: 862c9d7bfc69b8d3193c56753c1988606ef8be5a

--- scripts/utils/upload-artifact.sh
+++ scripts/utils/upload-artifact.sh
@@ -43,7 +43,7 @@ UPLOAD_RESPONSE=$(curl -v -X PUT \
 
 if echo "$UPLOAD_RESPONSE" | grep -q "HTTP/[0-9.]* 200"; then
   echo -e "\033[32mUploaded build to Stainless storage.\033[0m"
-  echo -e "\033[32mInstallation: Download and unzip: 'https://pkg.stainless.com/s/openlayer-go/$SHA'. Run 'go mod edit -replace github.com/openlayer-ai/openlayer-go=/path/to/unzipped_directory'.\033[0m"
+  echo -e "\033[32mInstallation: Download and unzip: 'https://pkg.stainless.com/s/openlayer-go-staging/$SHA'. Run 'go mod edit -replace github.com/openlayer-ai/openlayer-go=/path/to/unzipped_directory'.\033[0m"
 else
   echo -e "\033[31mFailed to upload artifact.\033[0m"
   exit 1
